9d859df42e61521090cdee554dfde14bcb72fd1a
Sweetfearr <122605620+Sweetfearr@users.noreply.github.com>
2023-04-07 23:57:53 +0600

    Initial commit

---
+++ Filee.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class Filee {
+    public static void main(String[] args) {
+        File dir = new File("C://SomeDir");
+
+        System.out.println(dir.isDirectory());
+
+    }
+}
